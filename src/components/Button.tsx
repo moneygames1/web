1c084751cca532a212cbf5326868330bff4ea1d6
assassinxoxo <shubham.singh377@gmail.com>
2025-02-23 13:37:36 +0530

    testing deployment

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,27 @@
 const Button = ({
-	children,
-	extraClassName,
-	isDisabled,
-	...props
+  children,
+  extraClassName,
+  isDisabled,
+  ...props
 }: {
-	children: React.ReactNode;
-	extraClassName?: string;
+  children: React.ReactNode;
+  extraClassName?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement> & { [key: string]: any }) => {
-	return (
-		<button
-			{...props}
-			className={`uppercase p-2 pb-0 border-2 border-black rounded-lg -rotate-3 bg-secondary hover:rotate-0 ${extraClassName} ${
-				isDisabled
-					? '!bg-gray-500 hover:-rotate-3 !cursor-not-allowed'
-					: ''
-			}`}
-			disabled={isDisabled}
-			style={{
-				boxShadow: '0px 4px 0px 0px rgba(0, 0, 0, 1)',
-			}}
-		>
-			{children}
-		</button>
-	);
+  //adding testing comment
+  return (
+    <button
+      {...props}
+      className={`uppercase p-2 pb-0 border-2 border-black rounded-lg -rotate-3 bg-secondary hover:rotate-0 ${extraClassName} ${
+        isDisabled ? "!bg-gray-500 hover:-rotate-3 !cursor-not-allowed" : ""
+      }`}
+      disabled={isDisabled}
+      style={{
+        boxShadow: "0px 4px 0px 0px rgba(0, 0, 0, 1)",
+      }}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
